8b843776892c6d9d4910052c66ebd2c53d189aad
KanyaT <kanyathyagaraj@gmail.com>
2024-05-12 12:54:18 +0530

    cleared merge conflict

---
+++ main_BACKUP_1165.js
@@ -0,0 +1,7 @@
+var num=50;
+var truth="Hello cats"
+<<<<<<< HEAD
+var manifest="Hare Krishna Ram Ram"
+=======
+var manifest="Hare Krishna Hare Rama"
+>>>>>>> 5e4679b3d4e47c861445263821f9e3326ed5ef98
